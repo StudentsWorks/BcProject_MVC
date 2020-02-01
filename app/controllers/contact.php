909e6b9abb343a3f8dc69fa031d796975088ea5a
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2020-02-01 11:08:56 +0100

    first commit

---
+++ app/controllers/contact.php
@@ -0,0 +1,12 @@
+<?php
+    class Contact extends Controller {
+        
+        public function index() {
+            print "This is contact/index";
+        }
+
+        public function Phone() {
+            print "Contact by phone";
+        }
+
+    }
